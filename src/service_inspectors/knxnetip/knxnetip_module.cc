54a7d033a058df9fa2f456f54b9c4ceb5df77feb
Alija Sabic <sabic.alija@gmail.com>
2018-08-02 04:51:03 +0200

    Restructure knxnetip module: remove memory allocation with new and work with (dynamic) vector

--- src/service_inspectors/knxnetip/knxnetip_module.cc
+++ src/service_inspectors/knxnetip/knxnetip_module.cc
@@ -20,79 +20,85 @@
 #include "config.h"
 #endif
 
+#include <regex>
+
 #include "knxnetip_module.h"
 #include "knxnetip_module_enum.h"
 #include "knxnetip.h"
-#include "knxnetip_enum.h"
+#include "knxnetip_config.h"
 #include "knxnetip_regex.h"
+#include "knxnetip_tables.h"
 
 #include "profiler/profiler.h"
 #include "log/messages.h"
 
-#include <fstream>
-#include <regex>
-
 using namespace snort;
-using namespace KNXnetIPModuleEnums;
+THREAD_LOCAL ProfileStats knxnetip_prof;
+
+//-------------------------------------------------------------------------
+// stats
+//-------------------------------------------------------------------------
+const PegInfo* KNXnetIPModule::get_pegs() const
+{ return knxnetip::module::peg_names; }
+
+PegCount* KNXnetIPModule::get_counts() const
+{ return (PegCount*)&knxnetip_stats; }
+
+//-------------------------------------------------------------------------
+// rules
+//-------------------------------------------------------------------------
+const RuleMap* KNXnetIPModule::get_rules() const
+{ return knxnetip::module::rules; }
 
 //-------------------------------------------------------------------------
 // params
 //-------------------------------------------------------------------------
-KNXnetIPPolicyParaList *policy_worker = nullptr;
-KNXnetIPServerParaList *server_worker = nullptr;
+static knxnetip::module::policy *pworker;
+static knxnetip::module::server *sworker;
 
-KNXnetIPModule::KNXnetIPModule() : Module(KNXNETIP_NAME, KNXNETIP_HELP, knxnetip_params)
+KNXnetIPModule::KNXnetIPModule() : Module(KNXNETIP_NAME, KNXNETIP_HELP, knxnetip::module::params)
+{
+    pworker = nullptr;
+    sworker = nullptr;
+}
+KNXnetIPModule::~KNXnetIPModule()
 { }
 
-KNXnetIPModule::~KNXnetIPModule()
-{
-	if (server_worker)
-		delete server_worker;
-
-	if (policy_worker)
-	    delete policy_worker;
-
-	if (params)
-	    delete params;
-}
-
+//-------------------------------------------------------------------------
+// params - load configuration
+//-------------------------------------------------------------------------
 bool KNXnetIPModule::begin(const char *fqn, int idx, SnortConfig *)
 {
 	if (!idx && !strcmp(fqn, "knxnetip"))
-	{
-		// setup global config
-		delete params;
-		params = new KNXnetIPParaList;
-	}
+	{ }
 	else if (idx && !strcmp(fqn, "knxnetip.servers"))
 	{
 		// setup new server config
-		if (server_worker)
-			delete server_worker;
-		server_worker = new KNXnetIPServerParaList;
+	    params.servers.push_back(knxnetip::module::server{});
+	    sworker = &params.servers.back();
 	}
 	else if (idx && !strcmp(fqn, "knxnetip.policies"))
 	{
 		// setup new policy config
-		if (policy_worker)
-			delete policy_worker;
-		policy_worker = new KNXnetIPPolicyParaList;
+	    params.policies.push_back(knxnetip::module::policy{});
+	    pworker = &params.policies.back();
 	}
 
 	return true;
 }
-
 bool KNXnetIPModule::set(const char *fqn, Value& val, SnortConfig *sc)
 {
 	// global
 	if (val.is("global_policy"))
 	{
-		params->global_policy = val.get_long();
+		params.global_policy = val.get_long();
 	}
 
 	// server
-	else if (server_worker)
+	else if (sworker)
 	{
+	    knxnetip::module::server *sworker = &params.servers.back();
+
 		if (val.is("cidr"))
 		{
 			unsigned n;
@@ -103,48 +109,50 @@ bool KNXnetIPModule::set(const char *fqn, Value& val, SnortConfig *sc)
 
 			if(std::regex_search(s, m, r))
 			{
-			    server_worker->cidr.set(s.c_str());
+			    sworker->cidr.set(s.c_str());
 			}
 			else
 			{
                 LogMessage("ERROR: invalid ip address '%s'\n", s.c_str());
-                server_worker->cidr.set("0.0.0.0/32");
+                sworker->cidr.set("0.0.0.0/32");
 			}
 		}
 		else if (val.is("port"))
 		{
-			server_worker->ports.push_back(val.get_long());
+			sworker->ports.push_back(val.get_long());
 		}
 		else if (val.is("policy"))
 		{
-			server_worker->policy = val.get_long() - 1;
+			sworker->policy = val.get_long() - 1;
 		}
 	}
 
 	// policy
-	else if (policy_worker)
+	else if (pworker)
 	{
+	    knxnetip::module::policy *pworker = &params.policies.back();
+
 		if (val.is("individual_addressing"))
 		{
-			policy_worker->individual_addressing = val.get_bool();
+			pworker->individual_addressing = val.get_bool();
 		}
 		else if (val.is("payload"))
 		{
-			policy_worker->payload = val.get_bool();
+			pworker->payload = val.get_bool();
 		}
 		else if (val.is("group_addressing"))
 		{
-			policy_worker->group_addressing = val.get_bool();
+			pworker->group_addressing = val.get_bool();
 		}
 		else if (val.is("group_address_level"))
 		{
-		    policy_worker->group_address_level = val.get_long();
+		    pworker->group_address_level = val.get_long();
 		}
 		else if (val.is("group_address_file"))
 		{
 			unsigned n;
 			const uint8_t *b = val.get_buffer(n);
-			policy_worker->group_address_file.assign((const char *)b, n);
+			pworker->group_address_file.assign((const char *)b, n);
 		}
 		else if (val.is("services"))
 		{
@@ -152,7 +160,7 @@ bool KNXnetIPModule::set(const char *fqn, Value& val, SnortConfig *sc)
 			const uint8_t *b = val.get_buffer(n);
 			std::string s;
 			s.assign((const char *)b, n);
-			policy_worker->services.push_back(s);
+			pworker->services.push_back(s);
 		}
 
 	}
@@ -164,7 +172,7 @@ bool KNXnetIPModule::end(const char *fqn, int idx, SnortConfig *)
 	// (global) configuration
 	if (!idx && !strcmp(fqn, "knxnetip"))
 	{
-		if (!validate(this->params))
+		if (!knxnetip::module::validate(params))
 		{
 			// FIXIT-L: Replace with proper snort message handling.
 			LogMessage("ERROR: knxnetip configuration failed\n");
@@ -174,229 +182,20 @@ bool KNXnetIPModule::end(const char *fqn, int idx, SnortConfig *)
 		}
 
 		// load group addresses, etc.
-		load(this->params);
+		knxnetip::module::load(params);
 	}
 
 	// server
 	else if (idx && !strcmp(fqn, "knxnetip.servers"))
 	{
-		if (!server_worker)
-		{
-			ParseError("invalid %s[%d]", fqn, idx);
-			return true;
-		}
-
-		params->servers.push_back(server_worker);
-		server_worker = nullptr;
+	    sworker = nullptr;
 	}
 
 	// policy
 	else if (idx && !strcmp(fqn, "knxnetip.policies"))
 	{
-		if (!policy_worker)
-		{
-			ParseError("invalid %s[%d]", fqn, idx);
-		}
-
-		params->policies.push_back(policy_worker);
-		policy_worker = nullptr;
+	    pworker = nullptr;
 	}
 
 	return true;
-}
-
-bool KNXnetIPModule::validate(KNXnetIPParaList *param)
-{
-
-	// validate server config
-	for (int i = 0; i < param->servers.size(); i++)
-	{
-	    KNXnetIPServerParaList *p {param->servers.at(i)};
-
-		// policy number
-		if (p->policy < 0 or p->policy > (param->policies.size()-1))
-		{
-		    LogMessage("ERROR: invalid policy '%d' at server[%d]\n", p->policy+1, i+1);
-			return false;
-		}
-
-	}
-
-	// validate policy config
-	for (int i = 0; i < param->policies.size(); i++)
-	{
-	    KNXnetIPPolicyParaList *p {param->policies.at(i)};
-
-		// group address file
-		std::ifstream ifile(p->group_address_file.c_str());
-		if (!ifile)
-		{
-			LogMessage("ERROR: invalid group address file '%s'\n", p->group_address_file.c_str());
-			p->group_address_file.clear();
-		}
-
-		// services
-		for (int j = 0; j < p->services.size(); j++)
-		{
-		    std::string s {p->services.at(j)};
-		    bool found = false;
-
-			for (int k = 0; knxnetip::service_identifier[k].text != nullptr; k++)
-			{
-			    if (!strcmp(s.c_str(), knxnetip::service_identifier[k].text))
-			    {
-			        found = true;
-			        break;
-			    }
-			}
-
-			if (!found)
-			{
-			    LogMessage("ERROR: invalid service '%s'\n", s.c_str());
-			    s.clear();
-			}
-		}
-
-	}
-
-	return true;
-}
-
-// FIXIT-M: Move to knxnetip related processing
-static uint16_t get_group_address(std::string m, std::string s)
-{
-    return (((uint16_t)std::stoi(m))  << 11) |
-            ((uint16_t)std::stoi(s));
-}
-
-// FIXIT-M: Move to knxnetip related processing
-static uint16_t get_group_address(std::string m, std::string mid, std::string s)
-{
-    return (((uint16_t)std::stoi(m))  << 11) |
-           (((uint16_t)std::stoi(mid)) << 8) |
-            ((uint16_t)std::stoi(s));
-}
-
-bool KNXnetIPModule::load(KNXnetIPParaList *param)
-{
-
-    // server
-//    for (int i = 0; i < param->servers.size(); i++)
-//    {
-//
-//    }
-
-    // policy
-    for (int i = 0; i < param->policies.size(); i++)
-    {
-        KNXnetIPPolicyParaList *p {param->policies.at(i)};
-
-        if (!p->group_address_file.empty())
-        {
-            std::string regexp;
-            if (p->group_address_level == 2)
-                regexp.assign(KNXNETIP_GRP_ADDR_2_REGEX);
-            else
-                regexp.assign(KNXNETIP_GRP_ADDR_3_REGEX);
-
-            std::regex grpaddr {regexp.c_str()};
-            std::smatch matches;
-
-            std::ifstream in(p->group_address_file.c_str());
-            std::string line;
-            while (std::getline(in, line))
-            {
-                if(std::regex_search(line, matches, grpaddr))
-                {
-                    if(p->group_address_level == 2)
-                    {
-                        p->group_addresses.push_back(
-                            get_group_address(
-                                matches[3].str(),
-                                matches[4].str()));
-                    }
-                    else
-                    {
-                        p->group_addresses.push_back(
-                            get_group_address(
-                                matches[3].str(),
-                                matches[4].str(),
-                                matches[5].str()));
-                    }
-                }
-            }
-        }
-    }
-
-    return true;
-}
-
-//-------------------------------------------------------------------------
-// commands
-//-------------------------------------------------------------------------
-const Command *KNXnetIPModule::get_commands() const
-{
-
-
-    return new Command;
-}
-
-
-//-------------------------------------------------------------------------
-// stats
-//-------------------------------------------------------------------------
-unsigned KNXnetIPModule::get_gid() const
-{
-    return GID_KNXNETIP;
-}
-
-THREAD_LOCAL ProfileStats KNXnetIPModule::knxnetip_profile;
-ProfileStats* KNXnetIPModule::get_profile() const
-{
-    return &knxnetip_profile;
-}
-
-Module::Usage KNXnetIPModule::get_usage() const
-{
-    return INSPECT;
-}
-
-//-------------------------------------------------------------------------
-// pegs
-//-------------------------------------------------------------------------
-const PegInfo* KNXnetIPModule::get_pegs() const
-{
-    return peg_names;
-}
-
-THREAD_LOCAL PegCount KNXnetIPModule::peg_counts[PEG_COUNT_MAX] = { 0 };
-PegCount* KNXnetIPModule::get_counts() const
-{
-    return peg_counts;
-}
-
-//-------------------------------------------------------------------------
-// rules
-//-------------------------------------------------------------------------
-static const snort::RuleMap knxnetip_rules[] =
-{
-    { 0, nullptr }
-};
-
-const snort::RuleMap* KNXnetIPModule::get_rules() const
-{ return knxnetip_rules; }
-
-//-------------------------------------------------------------------------
-// custom
-//-------------------------------------------------------------------------
-ProfileStats& KNXnetIPModule::get_profile_stats()
-{
-    return knxnetip_profile;
-}
-
-const KNXnetIPParaList *KNXnetIPModule::get_params()
-{
-    KNXnetIPParaList *r = params;
-    params = nullptr;
-    return r;
 }
--- src/service_inspectors/knxnetip/knxnetip_module.h
+++ src/service_inspectors/knxnetip/knxnetip_module.h
@@ -20,61 +20,16 @@
 #define KNXNETIP_MODULE_H
 
 #include "framework/module.h"
-#include "sfip/sf_cidr.h"
-#include "sfip/sf_ip.h"
+#include "knxnetip_config.h"
 
-#define GID_KNXNETIP 146
+#define GID_KNXNETIP 147
 
 #define KNXNETIP_NAME "knxnetip"
 #define KNXNETIP_HELP "knxnetip inspection"
 
-using namespace snort;
+extern THREAD_LOCAL snort::ProfileStats knxnetip_prof;
 
-//-------------------------------------------------------------------------
-// knxnetip statistics
-//-------------------------------------------------------------------------
-struct KNXnetIPStats
-{
-    PegCount total_frames;
-};
-
-//extern THREAD_LOCAL KNXnetIPStats knxnetip_stats;
-
-//-------------------------------------------------------------------------
-// knxnetip module
-//-------------------------------------------------------------------------
-
-struct KNXnetIPPolicyParaList
-{
-public:
-	bool individual_addressing = false;
-	bool payload = false;
-	bool group_addressing = false;
-	int group_address_level = 3;
-	std::string group_address_file;
-	std::vector<std::string> services;
-
-// deduced
-	std::vector<uint16_t> group_addresses;
-};
-
-struct KNXnetIPServerParaList
-{
-public:
-    SfCidr cidr;
-	std::vector<int> ports;
-	int policy;
-};
-
-struct KNXnetIPParaList
-{
-public:
-	int global_policy;
-	std::vector<KNXnetIPPolicyParaList *> policies;
-	std::vector<KNXnetIPServerParaList *> servers;
-};
-
-class KNXnetIPModule : public Module
+class KNXnetIPModule : public snort::Module
 {
 public:
     KNXnetIPModule();
@@ -84,51 +39,17 @@ public:
     bool set(const char*, Value&, SnortConfig*) override;
     bool end(const char*, int, SnortConfig*) override;
 
-    unsigned get_gid() const override;
+    unsigned get_gid() const override
+    { return GID_KNXNETIP; }
 
-    const Command* get_commands() const override;
-    const RuleMap* get_rules() const override;
+    const snort::RuleMap* get_rules() const override;
     const PegInfo* get_pegs() const override;
-
-//    bool counts_need_prep() const override;
-//    void prep_counts() override;
-
     PegCount* get_counts() const override;
-//    PegCount get_global_count(const char* name) const override;
-//    int get_num_counts() const override;
-    ProfileStats* get_profile() const override;
-//    const char* get_defaults() const override;
-//    bool global_stats() const override;
 
-//    void sum_stats(bool accumulate_now_stats) override;
-//    void show_interval_stats(IndexVec&, FILE*) override;
-//    void show_stats() override;
-//    void reset_stats() override;
-//    void show_dynamic_stats() override;
+    Module::Usage get_usage() const override
+    { return INSPECT; }
 
-    Module::Usage get_usage() const override;
-
-    static ProfileStats& get_profile_stats();
-    const KNXnetIPParaList *get_params();
-
-
-#ifdef REG_TEST
-    static const PegInfo* get_peg_names() { return peg_names; }
-    static const PegCount* get_peg_counts() { return peg_counts; }
-    static void reset_peg_counts() { }
-#endif
-
-private:
-    static bool validate(KNXnetIPParaList *param);
-    static bool load(KNXnetIPParaList *param);
-
-    static const Parameter knxnetip_params[];
-    static const RuleMap knxnetip_events[];
-    KNXnetIPParaList *params = nullptr;
-    static const PegInfo peg_names[];
-    static THREAD_LOCAL ProfileStats knxnetip_profile;
-    static THREAD_LOCAL PegCount peg_counts[];
+    knxnetip::module::param params;
 };
-
 
 #endif // KNXNETIP_MODULE_H
