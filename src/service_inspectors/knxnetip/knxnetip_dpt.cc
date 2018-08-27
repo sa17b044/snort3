aaec0683006d1f25eba1710623a66be875a6eb5a
Alija Sabic <sabic.alija@gmail.com>
2018-08-27 03:12:36 +0200

    knxnetip dpt: implement dpt translator for main class: 1, 2, 3, 4 and 9; add dpt map for those classes

--- src/service_inspectors/knxnetip/knxnetip_dpt.cc
+++ src/service_inspectors/knxnetip/knxnetip_dpt.cc
@@ -21,6 +21,141 @@
 #endif
 
 #include "knxnetip_dpt.h"
+#include "knxnetip_tables.h"
+#include "knxnetip_detect.h"
+
+#include <iomanip>
+#include <sstream>
+
+using knxnetip::dpt::Type;
+using knxnetip::dpt::DPT;
+
+//const std::map<Type, DPT> knxnetip::dpt::map
+const std::map<Type, DPT> map
+{
+    { Type::DPT_SWITCH,                 { "1.001", "Switch",                    "off",                  "on",                           "" } },
+    { Type::DPT_BOOL,                   { "1.002", "Boolean",                   "false",                "true",                         "" } },
+    { Type::DPT_ENABLE,                 { "1.003", "Enable",                    "disable",              "enable",                       "" } },
+    { Type::DPT_RAMP,                   { "1.004", "Ramp",                      "no ramp",              "ramp",                         "" } },
+    { Type::DPT_ALARM,                  { "1.005", "Alarm",                     "no alarm",             "alarm",                        "" } },
+    { Type::DPT_BINARYVALUE,            { "1.006", "Binary value",              "low",                  "high",                         "" } },
+    { Type::DPT_STEP,                   { "1.007", "Step",                      "decrease",             "increase",                     "" } },
+    { Type::DPT_UPDOWN,                 { "1.008", "Up/Down",                   "up",                   "down",                         "" } },
+    { Type::DPT_OPENCLOSE,              { "1.009", "Open/Close",                "open",                 "close",                        "" } },
+    { Type::DPT_START,                  { "1.010", "Start",                     "stop",                 "start",                        "" } },
+    { Type::DPT_STATE,                  { "1.011", "State",                     "inactive",             "active",                       "" } },
+    { Type::DPT_INVERT,                 { "1.012", "Invert",                    "not inverted",         "inverted",                     "" } },
+    { Type::DPT_DIMSENDSTYLE,           { "1.013", "Dim send-style",            "start/stop",           "cyclic",                       "" } },
+    { Type::DPT_INPUTSOURCE,            { "1.014", "Input source",              "fixed",                "calculated",                   "" } },
+    { Type::DPT_RESET,                  { "1.015", "Reset",                     "no action",            "reset",                        "" } },
+    { Type::DPT_ACK,                    { "1.016", "Acknowledge",               "no action",            "acknowledge",                  "" } },
+    { Type::DPT_TRIGGER,                { "1.017", "Trigger",                   "trigger",              "trigger",                      "" } },
+    { Type::DPT_OCCUPANCY,              { "1.018", "Occupancy",                 "not occupied",         "occupied",                     "" } },
+    { Type::DPT_WINDOW_DOOR,            { "1.019", "Window/Door",               "closed",               "open",                         "" } },
+    { Type::DPT_LOGICAL_FUNCTION,       { "1.021", "Logical function",          "OR",                   "AND",                          "" } },
+    { Type::DPT_SCENE_AB,               { "1.022", "Scene A/B",                 "scene A",              "scene B",                      "" } },
+    { Type::DPT_SHUTTER_BLINDS_MODE,    { "1.023", "Shutter/Blinds mode",       "only move up/down",    "move up/down + step-stop",     "" } },
+    { Type::DPT_HEAT_COOL,              { "1.100", "Heat/Cool",                 "cooling",              "heating",                      "" } },
+    { Type::DPT_SWITCH_CONTROL,         { "2.001", "Switch Controlled",         "off",                  "on",                           "" } },
+    { Type::DPT_BOOL_CONTROL,           { "2.002", "Boolean Controlled",        "false",                "true",                         "" } },
+    { Type::DPT_ENABLE_CONTROL,         { "2.003", "Enable Controlled",         "disable",              "enable",                       "" } },
+    { Type::DPT_RAMP_CONTROL,           { "2.004", "Ramp Controlled",           "no ramp",              "ramp",                         "" } },
+    { Type::DPT_ALARM_CONTROL,          { "2.005", "Alarm Controlled",          "no alarm",             "alarm",                        "" } },
+    { Type::DPT_BINARY_CONTROL,         { "2.006", "Binary Controlled",         "low",                  "high",                         "" } },
+    { Type::DPT_STEP_CONTROL,           { "2.007", "Step Controlled",           "decrease",             "increase",                     "" } },
+    { Type::DPT_UPDOWN_CONTROL,         { "2.008", "Up/Down Controlled",        "up",                   "down",                         "" } },
+    { Type::DPT_OPENCLOSE_CONTROL,      { "2.009", "Open/Close Controlled",     "open",                 "close",                        "" } },
+    { Type::DPT_START_CONTROL,          { "2.010", "Start Controlled",          "stop",                 "start",                        "" } },
+    { Type::DPT_STATE_CONTROL,          { "2.011", "State Controlled",          "inactive",             "active",                       "" } },
+    { Type::DPT_INVERT_CONTROL,         { "2.012", "Invert Controlled",         "not inverted",         "inverted",                     "" } },
+    { Type::DPT_CONTROL_DIMMING,        { "3.007", "Dimming",                   "decrease",             "increase",                     "" } },
+    { Type::DPT_CONTROL_BLINDS,         { "3.008", "Up/Down",                   "up",                   "down",                         "" } },
+    { Type::DPT_TEMPERATURE,            { "9.001", "Temperature",               "-273",                 "+670760",                      "\u00b0C" } },
+    { Type::DPT_TEMPERATURE_DIFFERENCE, { "9.002", "Temperature difference",    "-670760",              "+670760",                      "K" } },
+    { Type::DPT_TEMPERATURE_GRADIENT,   { "9.003", "Temperature gradient",      "-670760",              "+670760",                      "K/h" } },
+    { Type::DPT_INTENSITY_OF_LIGHT,     { "9.004", "Light intensity",           "0",                    "+670760",                      "lx" } },
+    { Type::DPT_WIND_SPEED,             { "9.005", "Wind speed",                "0",                    "+670760",                      "m/s" } },
+    { Type::DPT_AIR_PRESSURE,           { "9.006", "Air pressure",              "0",                    "+670760",                      "Pa" } },
+    { Type::DPT_HUMIDITY,               { "9.007", "Humidity",                  "0",                    "+670760",                      "%%" } },
+    { Type::DPT_AIRQUALITY,             { "9.008", "Air quality",               "0",                    "+670760",                      "ppm" } },
+    { Type::DPT_AIR_FLOW,               { "9.009", "Air flow",                  "-670760",              "+670760",                      "m\u00b3/h" } },
+    { Type::DPT_TIME_DIFFERENCE,        { "9.010", "Time difference 1",         "-670760",              "+670760",                      "s" } },
+    { Type::DPT_TIME_DIFFERENCE2,       { "9.011", "Time difference 2",         "-670760",              "+670760",                      "ms" } },
+    { Type::DPT_VOLTAGE,                { "9.020", "Voltage",                   "-670760",              "+670760",                      "mV" } },
+    { Type::DPT_ELECTRICAL_CURRENT,     { "9.021", "Electrical current",        "-670760",              "+670760",                      "mA" } },
+    { Type::DPT_POWERDENSITY,           { "9.022", "Power density",             "-670760",              "+670760",                      "W/m\u00b2" } },
+    { Type::DPT_KELVIN_PER_PERCENT,     { "9.023", "Kelvin/percent",            "-670760",              "+670760",                      "K/%%" } },
+    { Type::DPT_POWER,                  { "9.024", "Power",                     "-670760",              "+670760",                      "kW" } },
+    { Type::DPT_VOLUME_FLOW,            { "9.025", "Volume flow",               "-670760",              "+670760",                      "l/h" } },
+    { Type::DPT_RAIN_AMOUNT,            { "9.026", "Rain amount",               "-671088.64",           "670760.96",                    "l/m\u00b2" } },
+    { Type::DPT_TEMP_F,                 { "9.027", "Temperature",               "-459.6",               "670760.96",                    "\u00b0F" } },
+    { Type::DPT_WIND_SPEED,             { "9.028", "Wind speed",                "0",                    "670760.96",                    "km/h" } }
+};
+
+
+
+void knxnetip::dpt::DPTXlator::set_type(uint32_t id)
+{
+    Type t = static_cast<Type>(id);
+    if (map.find(t) != map.end()){
+        type = map.find(t)->second;
+    }
+}
+
+knxnetip::dpt::DPTXlatorBoolean::DPTXlatorBoolean(uint32_t id, const uint8_t* data)
+{
+    set_type(id);
+    this->data = data;
+}
+
+std::string knxnetip::dpt::DPTXlatorBoolean::get_value()
+{
+    return (*data) & 0x01 ? type.lower : type.upper;
+}
+
+knxnetip::dpt::DPTXlator1BitControlled::DPTXlator1BitControlled(uint32_t id, const uint8_t* data)
+{
+    set_type(id);
+    this->data = data;
+}
+
+std::string knxnetip::dpt::DPTXlator1BitControlled::get_value()
+{
+    std::stringstream ss;
+
+    ss << ((*data) & 0x02) ? "1 " : "0 ";
+    ss << ((*data) & 0x01) ? type.lower : type.upper;
+
+    return ss.str();
+}
+
+knxnetip::dpt::DPTXlator3BitControlled::DPTXlator3BitControlled(uint32_t id, const uint8_t* data)
+{
+    set_type(id);
+    this->data = data;
+}
+
+std::string knxnetip::dpt::DPTXlator3BitControlled::get_value()
+{
+    std::stringstream ss;
+    int steps = (*data) & 0x7;
+
+    ss << ((*data) & 0x8) ? type.lower : type.upper;
+    ss << " ";
+    
+    if (!steps)
+        ss << "break";
+    else
+        ss << steps << " steps";
+
+    return ss.str();
+}
+
+knxnetip::dpt::DPTXlator2ByteFloat::DPTXlator2ByteFloat(uint32_t id, const uint16_t* data)
+{
+    set_type(id);
+    this->data = data;
+}
 
 // DPT 2 Byte Float= (0.01 * m) * 2^e
 static double get_2bytefloat(uint16_t v)
@@ -36,121 +171,88 @@ static double get_2bytefloat(uint16_t v)
     return (1 << exp) * m * 0.01;
 }
 
-bool knxnetip::dpt::is_dpt_higher(knxnetip::packet::cemi::apdu::GroupValue& gv, uint32_t dpt, double max)
+
+std::string knxnetip::dpt::DPTXlator2ByteFloat::get_value()
 {
-    bool result = false;
+    uint16_t v = ntohs(*data);
+    double d = get_2bytefloat(v);
 
-    uint8_t main = (dpt & 0xffff0000) >> 16;
+    std::stringstream ss;
+    ss << d;
 
-    switch (main)
-    {
-        /* DPT 1.+++ */
-        case 1:
-            break;
-
-        /* DPT 2.+++ */
-        case 2:
-            break;
-
-        /* DPT 3.+++ */
-        case 3:
-            break;
-
-        /* DPT 4.+++ */
-        case 4:
-            break;
-
-        /* DPT 5.+++ */
-        case 5:
-            break;
-
-        /* DPT 6.+++ */
-        case 6:
-            break;
-
-        /* DPT 7.+++ */
-        case 7:
-            break;
-
-        /* DPT 8.+++ */
-        case 8:
-            break;
-
-        /* DPT 9.+++ */
-        case 9:
-            if (gv.length == 2)
-            {
-                uint16_t v = ((uint16_t) gv.get_data(0)) << 8 | (uint16_t) gv.get_data(1);
-                double dpt_val = get_2bytefloat(v);
-                if ( dpt_val > max)
-                {
-                    result = true;
-                }
-            }
-            break;
-
-        default:
-            break;
-    }
-
-    return result;
+    return ss.str();
 }
 
-bool knxnetip::dpt::is_dpt_lower(knxnetip::packet::cemi::apdu::GroupValue& gv, uint32_t dpt, double min)
+
+/* FIXME-BE: max, value only work for DPT 9 classes. */
+bool knxnetip::dpt::is_extrema(knxnetip::packet::cemi::apdu::GroupValue& gv, knxnetip::module::Spec spec, const snort::Packet& p, knxnetip::module::server& server, const knxnetip::module::policy& policy)
 {
+    using knxnetip::detection::get_rule_string;
+    using snort::TextLog_Flush;
+    using snort::TextLog_Print;
+
     bool result = false;
+    uint8_t main = (spec.dpt & 0xffff0000) >> 16;
 
-    uint8_t main = (dpt & 0xffff0000) >> 16;
+    std::string unit{}, value{}, extrema{}, rulestring{};
+    unsigned sid;
 
-    switch (main)
+    /* DPT 1.+++ */
+    if (main == 1) { }
+    /* DPT 2.+++ */
+    else if (main == 2) { }
+    /* DPT 3.+++ */
+    else if (main == 3) { }
+    /* DPT 4.+++ */
+    else if (main == 4) { }
+    /* DPT 5.+++ */
+    else if (main == 5) { }
+    /* DPT 6.+++ */
+    else if (main == 6) { }
+    /* DPT 7.+++ */
+    else if (main == 7) { }
+    /* DPT 8.+++ */
+    else if (main == 8) { }
+    /* DPT 9: 2 Byte Float */
+    else if (main == 9) {
+        knxnetip::dpt::DPTXlator2ByteFloat dptx{spec.dpt, (const uint16_t*)gv.data};
+        value = dptx.get_value();
+        unit = dptx.type.unit;
+        double v = std::stod(value);
+
+        if (spec.get_state(Spec::State::MAX) and v > spec.max)
+        {
+            result = true;
+            sid = KNXNETIP_GRPADDR_MAX;
+            rulestring = KNXNETIP_GRPADDR_MAX_STR_PAR;
+
+            std::stringstream ss;
+            ss << std::fixed << std::setprecision(2) << spec.max;
+            extrema = ss.str();
+        }
+        else if (spec.get_state(Spec::State::MIN) and v > spec.min)
+        {
+            result = true;
+            sid = KNXNETIP_GRPADDR_MIN;
+            rulestring = KNXNETIP_GRPADDR_MIN_STR_PAR;
+
+            std::stringstream ss;
+            ss << std::fixed << std::setprecision(2) << spec.min;
+            extrema = ss.str();
+        }
+    }
+
+    if (result)
     {
-        /* DPT 1.+++ */
-        case 1:
-            break;
-
-        /* DPT 2.+++ */
-        case 2:
-            break;
-
-        /* DPT 3.+++ */
-        case 3:
-            break;
-
-        /* DPT 4.+++ */
-        case 4:
-            break;
-
-        /* DPT 5.+++ */
-        case 5:
-            break;
-
-        /* DPT 6.+++ */
-        case 6:
-            break;
-
-        /* DPT 7.+++ */
-        case 7:
-            break;
-
-        /* DPT 8.+++ */
-        case 8:
-            break;
-
-        /* DPT 9.+++ */
-        case 9:
-            if (gv.length == 2)
-            {
-                uint16_t v = ((uint16_t) gv.get_data(0)) << 8 | (uint16_t) gv.get_data(1);
-                double dpt_val = get_2bytefloat(v);
-                if ( dpt_val < min)
-                {
-                    result = true;
-                }
-            }
-            break;
-
-        default:
-            break;
+        knxnetip::queue_det_event(sid, p, server, policy);
+        if (server.log_knxnetip)
+        {
+            std::string rule = get_rule_string(rulestring, server.log_to_file);
+            TextLog_Print(server.log, rule.c_str(), GID_KNXNETIP, sid, 0,
+                                      value.c_str(), unit.c_str(), extrema.c_str(), unit.c_str());
+            TextLog_NewLine(server.log);
+            TextLog_Flush(server.log);
+        }
     }
 
     return result;
--- src/service_inspectors/knxnetip/knxnetip_dpt.h
+++ src/service_inspectors/knxnetip/knxnetip_dpt.h
@@ -19,19 +19,139 @@
 #ifndef KNXNETIP_DPT_H
 #define KNXNETIP_DPT_H
 
+#include <string>
 #include "knxnetip_apdu.h"
+#include "knxnetip_module.h"
 
 namespace knxnetip
 {
 
     namespace dpt
     {
-        bool is_dpt_higher(knxnetip::packet::cemi::apdu::GroupValue& gv, uint32_t dpt, double max);
-        bool is_dpt_lower(knxnetip::packet::cemi::apdu::GroupValue& gv, uint32_t dpt, double min);
+        enum class Type : uint32_t
+        {
+            DPT_SWITCH                  = 0x00010001,
+            DPT_BOOL                    = 0x00010002,
+            DPT_ENABLE                  = 0x00010003,
+            DPT_RAMP                    = 0x00010004,
+            DPT_ALARM                   = 0x00010005,
+            DPT_BINARYVALUE             = 0x00010006,
+            DPT_STEP                    = 0x00010007,
+            DPT_UPDOWN                  = 0x00010008,
+            DPT_OPENCLOSE               = 0x00010009,
+            DPT_START                   = 0x0001000a,
+            DPT_STATE                   = 0x0001000b,
+            DPT_INVERT                  = 0x0001000c,
+            DPT_DIMSENDSTYLE            = 0x0001000d,
+            DPT_INPUTSOURCE             = 0x0001000e,
+            DPT_RESET                   = 0x0001000f,
+            DPT_ACK                     = 0x00010010,
+            DPT_TRIGGER                 = 0x00010011,
+            DPT_OCCUPANCY               = 0x00010012,
+            DPT_WINDOW_DOOR             = 0x00010013,
+            DPT_LOGICAL_FUNCTION        = 0x00010015,
+            DPT_SCENE_AB                = 0x00010016,
+            DPT_SHUTTER_BLINDS_MODE     = 0x00010017,
+            DPT_HEAT_COOL               = 0x00010064,
+            DPT_SWITCH_CONTROL          = 0x00020001,
+            DPT_BOOL_CONTROL            = 0x00020002,
+            DPT_ENABLE_CONTROL          = 0x00020003,
+            DPT_RAMP_CONTROL            = 0x00020004,
+            DPT_ALARM_CONTROL           = 0x00020005,
+            DPT_BINARY_CONTROL          = 0x00020006,
+            DPT_STEP_CONTROL            = 0x00020007,
+            DPT_UPDOWN_CONTROL          = 0x00020008,
+            DPT_OPENCLOSE_CONTROL       = 0x00020009,
+            DPT_START_CONTROL           = 0x0002000a,
+            DPT_STATE_CONTROL           = 0x0002000b,
+            DPT_INVERT_CONTROL          = 0x0002000c,
+            DPT_CONTROL_DIMMING         = 0x00030007,
+            DPT_CONTROL_BLINDS          = 0x00030008,
+            DPT_TEMPERATURE             = 0x00090001,
+            DPT_TEMPERATURE_DIFFERENCE  = 0x00090002,
+            DPT_TEMPERATURE_GRADIENT    = 0x00090003,
+            DPT_INTENSITY_OF_LIGHT      = 0x00090004,
+            DPT_WIND_SPEED              = 0x00090005,
+            DPT_AIR_PRESSURE            = 0x00090006,
+            DPT_HUMIDITY                = 0x00090007,
+            DPT_AIRQUALITY              = 0x00090008,
+            DPT_AIR_FLOW                = 0x00090009,
+            DPT_TIME_DIFFERENCE         = 0x0009000a,
+            DPT_TIME_DIFFERENCE2        = 0x0009000b,
+            DPT_VOLTAGE                 = 0x00090014,
+            DPT_ELECTRICAL_CURRENT      = 0x00090015,
+            DPT_POWERDENSITY            = 0x00090016,
+            DPT_KELVIN_PER_PERCENT      = 0x00090017,
+            DPT_POWER                   = 0x00090018,
+            DPT_VOLUME_FLOW             = 0x00090019,
+            DPT_RAIN_AMOUNT             = 0x0009001a,
+            DPT_TEMP_F                  = 0x0009001b,
+            DPT_WIND_SPEED_KMH          = 0x0009001c
+        };
 
+        struct DPT
+        {
+            std::string id;
+            std::string desc;
+            std::string lower;
+            std::string upper;
+            std::string unit;
 
+            DPT() {}
+            DPT(std::string id, std::string description, std::string lower, std::string upper) :
+                id{id}, desc{description}, lower{lower}, upper{upper} {}
+            DPT(std::string id, std::string description, std::string lower, std::string upper, std::string unit) :
+                id{id}, desc{description}, lower{lower}, upper{upper}, unit{unit} {}
+        };
+
+        struct DPTXlator
+        {
+            DPT type {};
+
+            void set_type(uint32_t id);
+        };
+
+        class DPTXlatorBoolean : public DPTXlator
+        {
+            const uint8_t* data;
+        public:
+            DPTXlatorBoolean(uint32_t id, const uint8_t* data);
+            std::string get_value();
+        };
+
+        class DPTXlator1BitControlled : public DPTXlator
+        {
+            const uint8_t* data;
+        public:
+            DPTXlator1BitControlled(uint32_t id, const uint8_t* data);
+            std::string get_value();
+        };
+
+        class DPTXlator3BitControlled : public DPTXlator
+        {
+            const uint8_t* data;
+        public:
+            DPTXlator3BitControlled(uint32_t id, const uint8_t* data);
+            std::string get_value();
+        };
+
+        class DPTXlator2ByteFloat : public DPTXlator
+        {
+            const uint16_t* data;
+        public:
+            DPTXlator2ByteFloat(uint32_t id, const uint16_t* data);
+            std::string get_value();
+        };
+
+        using knxnetip::packet::cemi::apdu::GroupValue;
+        using knxnetip::module::Spec;
+        using knxnetip::module::server;
+        using knxnetip::module::policy;
+        using snort::Packet;
+
+        bool is_extrema(GroupValue& gv, Spec spec, const Packet& p, server& server, const policy& policy);
+//        extern const std::map<knxnetip::dpt::Type, knxnetip::dpt::DPT> map;
     }
-
 
 }
 
